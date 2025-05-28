5e0586e8c1cd89b47c25fa7734a77dac18ee148d
Joao Victor <joaovicamposjf@gmail.com>
2025-05-28 16:01:02 -0300

    inicia-projeto

---
+++ api-wallet/src/main/java/br/com/my_wallett/api_wallet/ApiWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.my_wallett.api_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWalletApplication.class, args);
+	}
+
+}
---
+++ api-wallet/src/test/java/br/com/my_wallett/api_wallet/ApiWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.my_wallett.api_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
